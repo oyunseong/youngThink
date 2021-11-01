8746b37f5b723dfa9dff9b11b2a2ebafa2d6d9d5
oyunseong <gkrlsanjf@naver.com>
2021-11-01 15:21:58 +0900

    BaseClass 생성 및 정의

---
+++ app/src/main/java/com/youngthink/android/youngthink/base/BaseActivity.kt
@@ -0,0 +1,58 @@
+package com.youngthink.android.youngthink.base
+
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.viewbinding.ViewBinding
+
+abstract class BaseActivity<B: ViewBinding>(val bindingFactory: (LayoutInflater) -> B): AppCompatActivity(){
+    private var _binding: B? =null
+    val binding get() = _binding!!
+
+    private val tag: String = this::class.java.name
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Log.d(tag, "++onCreate")
+        _binding = bindingFactory(layoutInflater)
+        setContentView(binding.root)
+    }
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(tag, "++onDestroy")
+        _binding = null
+    }
+
+    fun showToast(str: String) {
+        Toast.makeText(this, str, Toast.LENGTH_SHORT).show();
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(tag, "++onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(tag, "++onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(tag, "++onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(tag, "++onStop")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d(tag, "++onRestart")
+    }
+
+
+}
